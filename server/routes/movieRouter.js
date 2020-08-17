b870b8ec42afeb2bd7f8c2fa7c1daf2d7137b066
Pardeep Singh <pardeeps09809@gmail.com>
2020-08-17 21:16:47 +0530

    movie api created

--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mkdirp = require('mkdirp');
+const { v4:uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const movieSchema = require('../models/movieSchema');
 
@@ -7,32 +10,198 @@ const movieSchema = require('../models/movieSchema');
 
 // route -> '/api/movie/getAll/movieDetail'
 // fetch all movie
-router.get('/getAll/movieDetail', (req, res) => {
-    res.send('movie api work')
+router.get('/getAll/movieDetail', async(req, res) => {
+    try {
+        movieSchema.find().exec()
+        .then(mov => {
+            if(!mov || mov.length == 0) {
+                return res.json({success: false, message: 'Movie list is empty'})
+            };
+
+            res.json({success: true, data: mov})
+        })
+        .catch(err => {
+            res.json({success: false, message: "Something went wrong", error: err});
+        });
+
+    } catch (err) {
+        res.json({ success: false, message: "Something went wrong", error: err });
+    };
 });
 
 // route -> '/api/movie/getOne/movieDetail/:movieId'
 // fetch one movie
-router.get('/getOne/movieDetail/:movieId', (req, res) => {
-    res.send('movie api work')
+router.get('/getOne/movieDetail/:movieId', async(req, res) => {
+    try {
+        const movieId = req.params.movieId;
+
+        const movie = await movieSchema.findById(movieId);
+        if(!movie) {
+            return res.json({success: false, message: `Movie id ${movieId} is not found`});
+        };
+
+        res.json({success: true, data: movie});
+
+    } catch (err) {
+        res.json({ success: false, message: "Something went wrong", error: err });
+    };
 });
 
 // route -> '/api/movie/add/movieDetail'
 // add new movie
-router.post('/add/movieDetail', (req, res) => {
-    res.send('movie api work')
+router.post('/add/movieDetail', async(req, res) => {
+    try {
+
+        let movieName = req.body.movieName;
+        if(!movieName) {
+            return res.json({success: false, message: "Movie name is required"});
+        };
+
+        const movie = await movieSchema.findOne({movieName: movieName});
+        if(movie) {
+            return res.json({success: false, message: "Movie name is already exist, choose another!"});
+        };
+        
+        let moviePoster;
+        if(req.files) {
+            let file = req.files.moviePoster;
+            moviePoster = 'uploads/moviePoster/' + uuidv4() + path.extname(file.name);
+
+            file.mv(`public/${moviePoster}`, (err) => {
+                if(err) return res.send({success:false, message: 'file uploading error'});
+    
+                console.log('file Uploaded')
+            });
+        };
+
+        let releasedDate = req.body.releasedDate; // date
+        let rated = req.body.rated;
+        let imbd_rating = req.body.imbd_rating; // number
+    
+        let genre = req.body.genre; // array
+        
+        let directorName = req.body.directorName; // array
+    
+        let writerName = req.body.writerName; // array
+    
+        let actorName = req.body.actorName; // array
+
+        movieSchema.create({
+            movieName: movieName,
+            moviePoster: moviePoster,
+            releasedDate: releasedDate, // date
+            rated: rated,
+            imbd_rating: imbd_rating, // number
+            genre: genre,
+            directorName: directorName,
+            writerName: writerName,
+            actorName: actorName
+            
+        })
+        .then(mov => {
+
+            res.json({success: true, message: 'Movie added', data: mov});
+
+        })
+        .catch(err => {
+            res.json({success: false, message: "Something went wrong", error: err});
+        });
+
+    } catch (err) {
+        res.json({ success: false, message: "Something went wrong", error: err });
+    };
 });
 
 // route -> '/api/movie/update/movieDetail/:movieId'
 // edit movie
-router.put('/update/movieDetail/:movieId', (req, res) => {
-    res.send('movie api work')
+router.put('/update/movieDetail/:movieId', async(req, res) => {
+    try {
+        const movieId = req.params.movieId;
+        
+        const movie = await movieSchema.findById(movieId);
+        if(!movie) {
+            return res.json({success: false, message: `Movie id ${movieId} is not found`});
+        };
+
+        let toUpdate = {};
+
+        if(req.body.movieName) {
+            const movieDetail = await movieSchema.findOne({movieName: req.body.movieName});
+            if(movieDetail) {
+                return res.json({success: false, message: "Movie name is already exist, choose another!"});
+            };
+            toUpdate['movieName'] = req.body.movieName
+        };
+        
+        if(req.files) {
+            let file = req.files.moviePoster;
+            toUpdate['moviePoster'] = 'uploads/moviePoster/' + uuidv4() + path.extname(file.name);
+            
+            file.mv(`public/${toUpdate.moviePoster}`, (err) => {
+                if(err) return res.send({success:false, message: 'file uploading error'});
+    
+                console.log('file Uploaded')
+            });
+
+            if(movie.moviePoster) {
+                if(fs.existsSync(`./public/${movie.moviePoster}`)) {
+                    fs.unlinkSync(`./public/${movie.moviePoster}`);
+                }
+            };
+        };
+
+        req.body.releasedDate ? ['releasedDate'] = req.body.releasedDate : ''; // date
+        req.body.rated        ? ['rated'] = req.body.rated : '';
+        req.body.imbd_rating  ? ['imbd_rating'] = req.body.imbd_rating : ''; // number
+    
+        req.body.genre        ? ['genre'] = req.body.genre : ''; // array
+        
+        req.body.directorName ? ['directorName'] = req.body.directorName : ''; // array
+    
+        req.body.writerName   ? ['writerName'] = req.body.writerName : ''; // array
+    
+        req.body.actorName    ? ['actorName'] = req.body.actorName : ''; // array
+
+        movieSchema.findByIdAndUpdate(movieId,{$set: toUpdate},{new:true}).exec()
+        .then(mov => {
+            res.json({success: true, message: 'Movie updated', data: mov})
+        })
+        .catch(err => {
+            res.json({success: false, message: "Something went wrong", error: err});
+        });
+
+    } catch (err) {
+        res.json({ success: false, message: "Something went wrong", error: err });
+    };
 });
 
 // route -> '/api/movie/update/movieDetail/:movieId'
 // delete movie
-router.delete('/delete/movieDetail/:movieId', (req, res) => {
-    res.send('movie api work')
+router.delete('/delete/movieDetail/:movieId', async(req, res) => {
+    try {
+        const movieId = req.params.movieId;
+
+        const movie = await movieSchema.findById(movieId);
+        if(!movie) {
+            return res.json({success: false, message: `Movie id ${movieId} is not found`});
+        };
+
+        movieSchema.findByIdAndDelete(movieId).exec()
+        .then(async(mov) => {
+            if(mov.moviePoster) {
+                if(fs.existsSync(`./public/${mov.moviePoster}`)) {
+                    fs.unlinkSync(`./public/${mov.moviePoster}`);
+                }
+            };
+            res.json({success: true, message: 'Movie detail deleted'})
+        })
+        .catch(err => {
+            res.json({success: false, message: "Something went wrong", error: err});
+        });
+
+    } catch (err) {
+        res.json({ success: false, message: "Something went wrong", error: err });
+    };
 });
 
 module.exports = router;
